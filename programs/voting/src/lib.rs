6020064531f3ca45b4032e32bad1df43ddeb9026
rahul-797 <rahul.sangwan797@gmail.com>
2025-12-05 19:43:48 +0530

    Basic structure; A simple contract and tests working

---
+++ programs/voting/src/lib.rs
@@ -0,0 +1,96 @@
+use anchor_lang::prelude::*;
+
+declare_id!("CkANkkC2LgoHpq3jLp5sgiGvxrEJEA76NZjS2NA8w8fr");
+
+#[program]
+pub mod voting {
+    use super::*;
+
+    pub fn initialize_poll(ctx: Context<InitializePoll>,
+                            poll_id: u64,
+                            description: String) -> Result<()> {
+        let poll = &mut ctx.accounts.poll;
+        poll.poll_id = poll_id;
+        poll.description = description;
+        poll.candidate_count = 0;
+        poll.poll_start = Clock::get()?.unix_timestamp as u64;
+        poll.poll_end = 0;
+        msg!("Greetings from {}", ctx.program_id);
+        msg!("Poll initialized with ID: {}", poll.poll_id);
+        msg!("Description: {}", poll.description);
+        msg!("Poll start time: {}", poll.poll_start);
+        Ok(())
+    }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>,
+                            name: String,
+                            _poll_id: u64,
+                            message: String) -> Result<()> {
+        let candidate = &mut ctx.accounts.candidate;
+        candidate.name = name;
+        candidate.message = message;
+        candidate.votes = 0;
+        msg!("Candidate initialized with Name: {}", candidate.name);
+        msg!("Votes: {}", candidate.votes);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64)]
+pub struct InitializePoll<'info> {
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = 8 + Poll::INIT_SPACE,
+        seeds = [b"poll", poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Poll {
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub candidate_count: u64,
+    pub poll_start: u64,
+    pub poll_end: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info> {
+    #[account(
+        mut,
+        seeds = [b"poll", poll_id.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub poll: Account<'info, Poll>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = 8 + Candidate::INIT_SPACE,
+        seeds = [b"candidate", poll_id.to_le_bytes().as_ref(), name.as_ref()],
+        bump,
+    )]
+    pub candidate: Account<'info, Candidate>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate {
+    #[max_len(16)]
+    pub name: String,
+    #[max_len(64)]
+    pub message: String,
+    pub votes: u64,
+}
---
+++ tests/voting.ts
@@ -0,0 +1,81 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { SystemProgram, Keypair } from "@solana/web3.js";
+import { expect } from "chai";
+
+import { Voting } from "../target/types/voting";
+
+describe("voting", () => {
+  // Provider & program
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Voting as Program<Voting>;
+  const walletPubkey = provider.wallet.publicKey;
+
+  console.log("Program ID from workspace:", program.programId.toBase58());
+
+  it("initializes a poll", async () => {
+  const pollId = new anchor.BN(1);
+  const description = "Anchor test poll";
+
+  const sig = await program.methods
+    .initializePoll(pollId, description)
+    .accounts({
+      user: walletPubkey,
+    })
+    .rpc();
+
+  console.log("initialize_poll tx:", sig);
+
+  // derive PDA exactly like Rust
+  const pollIdBuf = Buffer.alloc(8);
+  pollIdBuf.writeBigUInt64LE(BigInt(pollId.toString()));
+
+  const [pollPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("poll"), pollIdBuf],   // must match Rust seeds
+    program.programId
+  );
+
+  const poll = await program.account.poll.fetch(pollPda);
+
+  expect(poll.pollId.toNumber()).to.equal(1);
+  expect(poll.description).to.equal(description);
+  expect(poll.candidateCount.toNumber()).to.equal(0);
+  expect(poll.pollEnd.toNumber()).to.equal(0);
+  expect(poll.pollStart.toNumber()).to.be.greaterThan(0);
+});
+
+  it("initializes a candidate", async () => {
+    console.log("Program ID from workspace:", program.programId.toBase58());
+
+    const name = "Gvnsn";
+    const message = "BTC to the moon!";
+    const pollId = new anchor.BN(1); // u64
+
+    const sig = await program.methods
+      .initializeCandidate(name, pollId, message)
+      .accounts({
+        user: walletPubkey,
+      })
+      .rpc();
+
+    console.log("initialize_candidate tx:", sig);
+
+    // derive PDA exactly like Rust
+    const pollIdBuf = Buffer.alloc(8);
+    pollIdBuf.writeBigUInt64LE(BigInt(pollId.toString()));
+    const nameBuf = Buffer.from(name, "utf8");
+
+    const [pollPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("candidate"), pollIdBuf, nameBuf],   // must match Rust seeds
+      program.programId
+    );
+
+  const candidate = await program.account.candidate.fetch(pollPda);
+
+    expect(candidate.name).to.equal(name);
+    expect(candidate.message).to.equal(message);
+    expect(candidate.votes.toNumber()).to.equal(0);
+  });
+});
