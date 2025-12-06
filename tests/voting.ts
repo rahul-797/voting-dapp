75d6aa9217a9f149364b842f4a2eb8e6b1a2dc48
rahul-797 <rahul.sangwan797@gmail.com>
2025-12-06 10:57:46 +0530

    organised little bit

--- tests/voting.ts
+++ tests/voting.ts
@@ -15,75 +15,73 @@ describe("voting", () => {
 
   console.log("Program ID from workspace:", program.programId.toBase58());
 
-  const num = 4;
+  const num = 6;
+  const name = "Modi";
   const pollId = new anchor.BN(num);
 
-//   it("initializes a poll", async () => {
-//   const description = "Anchor test poll";
+  it("initializes a poll", async () => {
+  const description = "Anchor test poll";
 
-//   const sig = await program.methods
-//     .initializePoll(pollId, description)
-//     .accounts({
-//       user: walletPubkey,
-//     })
-//     .rpc();
+  const sig = await program.methods
+    .initializePoll(pollId, description)
+    .accounts({
+      user: walletPubkey,
+    })
+    .rpc();
 
-//   console.log("initialize_poll tx:", sig);
+  console.log("initialize_poll tx:", sig);
 
-//   // derive PDA exactly like Rust
-//   const pollIdBuf = Buffer.alloc(8);
-//   pollIdBuf.writeBigUInt64LE(BigInt(pollId.toString()));
+  // derive PDA exactly like Rust
+  const pollIdBuf = Buffer.alloc(8);
+  pollIdBuf.writeBigUInt64LE(BigInt(pollId.toString()));
 
-//   const [pollPda] = anchor.web3.PublicKey.findProgramAddressSync(
-//     [Buffer.from("poll"), pollIdBuf],   // must match Rust seeds
-//     program.programId
-//   );
+  const [pollPda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("poll"), pollIdBuf],   // must match Rust seeds
+    program.programId
+  );
 
-//   const poll = await program.account.poll.fetch(pollPda);
+  const poll = await program.account.poll.fetch(pollPda);
 
-//   expect(poll.pollId.toNumber()).to.equal(num);
-//   expect(poll.description).to.equal(description);
-//   expect(poll.candidateCount.toNumber()).to.equal(0);
-//   expect(poll.pollEnd.toNumber()).to.equal(0);
-//   expect(poll.pollStart.toNumber()).to.be.greaterThan(0);
-// });
+  expect(poll.pollId.toNumber()).to.equal(num);
+  expect(poll.description).to.equal(description);
+  expect(poll.candidateCount.toNumber()).to.equal(0);
+  expect(poll.pollEnd.toNumber()).to.equal(0);
+  expect(poll.pollStart.toNumber()).to.be.greaterThan(0);
+});
 
-//   it("initializes a candidate", async () => {
-//     console.log("Program ID from workspace:", program.programId.toBase58());
+  it("initializes a candidate", async () => {
+    console.log("Program ID from workspace:", program.programId.toBase58());
 
-//     const name = "Trump";
-//     const message = "BTC to the moon!";
+    const message = "BTC to the moon!";
 
-//     const sig = await program.methods
-//       .initializeCandidate(name, pollId, message)
-//       .accounts({
-//         user: walletPubkey,
-//       })
-//       .rpc();
+    const sig = await program.methods
+      .initializeCandidate(name, pollId, message)
+      .accounts({
+        user: walletPubkey,
+      })
+      .rpc();
 
-//     console.log("initialize_candidate tx:", sig);
+    console.log("initialize_candidate tx:", sig);
 
-//     // derive PDA exactly like Rust
-//     const pollIdBuf = Buffer.alloc(8);
-//     pollIdBuf.writeBigUInt64LE(BigInt(pollId.toString()));
-//     const nameBuf = Buffer.from(name, "utf8");
+    // derive PDA exactly like Rust
+    const pollIdBuf = Buffer.alloc(8);
+    pollIdBuf.writeBigUInt64LE(BigInt(pollId.toString()));
+    const nameBuf = Buffer.from(name, "utf8");
 
-//     const [pollPda] = anchor.web3.PublicKey.findProgramAddressSync(
-//       [Buffer.from("candidate"), pollIdBuf, nameBuf],   // must match Rust seeds
-//       program.programId
-//     );
+    const [pollPda] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("candidate"), pollIdBuf, nameBuf],   // must match Rust seeds
+      program.programId
+    );
 
-//   const candidate = await program.account.candidate.fetch(pollPda);
+  const candidate = await program.account.candidate.fetch(pollPda);
 
-//     expect(candidate.name).to.equal(name);
-//     expect(candidate.message).to.equal(message);
-//     expect(candidate.votes.toNumber()).to.equal(0);
-//   });
+    expect(candidate.name).to.equal(name);
+    expect(candidate.message).to.equal(message);
+    expect(candidate.votes.toNumber()).to.equal(0);
+  });
 
   it("voting", async () => {
     console.log("Program ID from workspace:", program.programId.toBase58());
-
-    const name = "Trump";
 
     const sig = await program.methods
       .vote(name, pollId)
